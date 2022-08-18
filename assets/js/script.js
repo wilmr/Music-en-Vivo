a8ff1221247cd4eca287ff718a3e957e63645d98
Wilmer Martinez <wmartinez.mail@gmail.com>
2022-08-18 03:48:27 -0700

    fixed css issues, ticketmaster api complete, working on saving to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@
 var nameInput = document.querySelector('#name-input');
 var eventCardTemplate = document.querySelector('[event-card-template]');
 var eventCardContainer = document.querySelector('[data-event-card-container]');
+var saveEvent = document.querySelector('[event-save]');
+var eventData = {};
+var savedEventData = [];
 
 
 var getEvents = function(artist) {
@@ -11,32 +14,28 @@ var getEvents = function(artist) {
     // make api request to the url
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            // console.log(data._embedded.events[0].name);
-            // console.log(data._embedded.events[0].dates.start.localDate);
-            // console.log(data._embedded.events[0]._embedded.venues[0].name);
-            // console.log(data._embedded.events[0].images[0].url)
             var event = data._embedded.events;
+            eventData = event;
+            var eventData_serailized = JSON.stringify(eventData);  
+            console.log(eventData_serailized);
             for (var i = 0; i < event.length; i++) {
                 const card = eventCardTemplate.content.cloneNode(true).children[0];
                 const header = card.querySelector("[event-name]");
                 const date = card.querySelector("[event-date]");
                 const venue = card.querySelector("[event-venue-name]");
-                const img = card.querySelector("[event-img]");          
+                const img = card.querySelector("[event-img]");
+                const url = card.querySelector("[event-url]");         
                 header.textContent = event[i].name;
                 date.textContent = event[i].dates.start.localDate;
                 venue.textContent = event[i]._embedded.venues[0].name;
                 img.src = event[i].images[0].url;
+                url.href = event[i].url;
                 eventCardContainer.append(card);
-            }
-        })
-            
-            // header.textContent = data._embedded.events[0].name;
-            // date.textContent = data._embedded.events[0].dates.start.localDate;
-            // venue.textContent = data._embedded.events[0]._embedded.venues[0].name;
-            // img.src = data._embedded.events[0].images[0].url;
-            
-    })
+            };
+        });
+    });
 };
+
 
 
 nameInput.addEventListener("keypress", (event) => {
@@ -47,4 +46,6 @@ nameInput.addEventListener("keypress", (event) => {
             
         };
 });
+
+
 
