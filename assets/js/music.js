047fec72632d26d2eb59c318877501e37c77c2d1
G Codes <graciela.mejia1@yahoo.com>
2022-08-18 18:37:41 -0700

    play button add

--- assets/js/music.js
+++ assets/js/music.js
@@ -10,6 +10,9 @@ window.onSpotifyWebPlaybackSDKReady = () => {
     },
     volume: 0.5,
   });
+  document.getElementById("togglePlay").onclick = function () {
+    player.togglePlay();
+  };
   container.append(player);
 };
 
@@ -19,7 +22,7 @@ fetch("https://api.spotify.com/v1/audio-analysis/6EJiVf7U0p1BBfs0qqeb1f", {
     Authorization: `Bearer BQD80VMxbL_y8O1x82iiMEis61m-OdS0W2hNepUo2i6ASClHxhBqWqs-mcje8zwCymP_EUSIBrDRUmIMPIDTcsrFG99mYaMK18LZwoqbKADUZYSlvTwoXARP8ExYOxfMk81Hip7d1479KWDE9MOTOJNljFlf7t2krYjKEpLZvx8SA0uyXPk1_NsrR3whg-RuEro1YB5Yp298RvND7xM`,
   },
 })
-  // .then((response) =>  response.json())
+  // .then((response) => response.json())
   .then(function (response) {
     return response.json();
   })
@@ -31,27 +34,5 @@ fetch("https://api.spotify.com/v1/audio-analysis/6EJiVf7U0p1BBfs0qqeb1f", {
   .catch((err) => {
     console.log(err);
   });
-
-// Ready
-player.addListener("ready", ({ device_id }) => {
-  console.log("Ready with Device ID", device_id);
-});
-
-// Not Ready
-player.addListener("not_ready", ({ device_id }) => {
-  console.log("Device ID has gone offline", device_id);
-});
-
-player.addListener("initialization_error", ({ message }) => {
-  console.error(message);
-});
-
-player.addListener("authentication_error", ({ message }) => {
-  console.error(message);
-});
-
-player.addListener("account_error", ({ message }) => {
-  console.error(message);
-});
 
 player.connect();
