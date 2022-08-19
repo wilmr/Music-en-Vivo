35e961fd1a1d4d55464eff5bf20a951adeff2dea
Wilmer Martinez <wmartinez.mail@gmail.com>
2022-08-18 18:29:27 -0700

    added search history

---
+++ assets/js/savedEvents.js
@@ -0,0 +1,33 @@
+
+var nameInput = document.querySelector('#name-input');
+var eventCardTemplate = document.querySelector('[event-card-template]');
+var eventCardContainer = document.querySelector('[data-event-card-container]');
+var saveEvent = document.querySelector('[event-save]');
+var savedEventData = localStorage.getItem("savedEvents");
+var parsedEventData = JSON.parse(savedEventData);
+
+
+console.log(savedEventData);
+
+
+
+            for (var i = 0; i < parsedEventData.length; i++) {
+                const card = eventCardTemplate.content.cloneNode(true).children[0];
+                const header = card.querySelector("[event-name]");
+                const date = card.querySelector("[event-date]");
+                const venue = card.querySelector("[event-venue-name]");
+                const img = card.querySelector("[event-img]");
+                const url = card.querySelector("[event-url]");      
+                header.textContent = parsedEventData[i].name;
+                date.textContent = parsedEventData[i].date;
+                venue.textContent = parsedEventData[i].venue;
+                img.src = parsedEventData[i].img;
+                //url.href = savedEventData[i].url;
+                eventCardContainer.append(card);              
+  
+            };
+
+
+
+
+
