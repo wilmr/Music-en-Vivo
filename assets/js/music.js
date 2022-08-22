1c94a44f9929a04652845b1077e49df8c264d9ae
G Codes <graciela.mejia1@yahoo.com>
2022-08-21 18:33:41 -0700

    billboard api changes

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,17 +1,30 @@
-var musicContainer = document.querySelector("#music-Container");
+var billboard = document.querySelector("#billboard-container");
+var rank = [];
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "1adbee6575msh71d2516c62383c1p144addjsn4d6d5a676fff",
-    "X-RapidAPI-Host": "billboard-api2.p.rapidapi.com",
-  },
+var getBillboard = function () {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "26541ea5b6dmshc516d2014468a79p12614ajsnecbfc71c931f",
+      "X-RapidAPI-Host": "billboard-api2.p.rapidapi.com",
+    },
+  };
+
+  fetch(
+    "https://billboard-api2.p.rapidapi.com/artist-100?range=1-10&date=2019-05-11",
+    options
+  ).then(function (response) {
+    response.json().then(function (data) {
+      var charts = data.content;
+
+      for (var i = 1; i <= 10; i++) {
+        rank.push(charts[i].artist);
+      }
+      var number = Math.floor(Math.random() * 10) + 1;
+      var randomName = rank[number];
+      console.log(randomName);
+    });
+  });
 };
 
-fetch(
-  "https://billboard-api2.p.rapidapi.com/artist-100?range=1-100&date=2019-05-11",
-  options
-)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+getBillboard();
